8f1a81720194e47c4ff92541f1d85536191a670d
Richard Yang <y.yang25@umiami.edu>
2017-08-21 22:53:45 -0400

    Rename MammalInt.java to MammalInt-Interface.java

---
+++ MammalInt-Interface.java
@@ -0,0 +1,30 @@
+/* 文件名: Animal.java */
+package animals;
+ 
+interface Animal {
+   public void eat();
+   public void travel();
+}
+package animals;
+ 
+/* 文件名 : MammalInt.java */
+public class MammalInt implements Animal{
+ 
+   public void eat(){
+      System.out.println("Mammal eats");
+   }
+ 
+   public void travel(){
+      System.out.println("Mammal travels");
+   } 
+ 
+   public int noOfLegs(){
+      return 0;
+   }
+ 
+   public static void main(String args[]){
+      MammalInt m = new MammalInt();
+      m.eat();
+      m.travel();
+   }
+}
