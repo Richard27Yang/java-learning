2f032a988c2d38e365ebd93eb6082a63f010a365
Richard Yang <y.yang25@umiami.edu>
2017-08-21 22:52:51 -0400

    Create Jave-Interface-Implements.java

---
+++ Jave-Interface-Implements.java
@@ -0,0 +1,24 @@
+// 文件名: Sports.java
+public interface Sports
+{
+   public void setHomeTeam(String name);
+   public void setVisitingTeam(String name);
+}
+ 
+// 文件名: Football.java
+public interface Football extends Sports
+{
+   public void homeTeamScored(int points);
+   public void visitingTeamScored(int points);
+   public void endOfQuarter(int quarter);
+}
+ 
+// 文件名: Hockey.java
+public interface Hockey extends Sports
+{
+   public void homeGoalScored();
+   public void visitingGoalScored();
+   public void endOfPeriod(int period);
+   public void overtimePeriod(int ot);
+}
+
